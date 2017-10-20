a8c5ba8b02cd6e526f02bed8b0f88d96d9b15ad3
mithsin <hsinbluemoon@gmail.com>
2017-10-20 16:41:57 -0400

    complete

--- keys.js
+++ keys.js
@@ -1,11 +1,24 @@
-
-console.log('this is loaded');
+/*console.log('this is loaded');*/
 
 var twitterKeys = {
   consumer_key: '4CLJiskLMV00Y5PyqMl26nzdD',
   consumer_secret: 'Bf1ddBtK0HL31O1TORgWtL1SGg50Kv45ICqat1p7UBW16nEpfN',
   access_token_key: '764567174564372480-WUoWSQrxaZQLEXh8mIsGEKPB47VJWWN',
   access_token_secret: 'ACYOWDiEeBRlWN0xSM4pMgDAda33SNkjaQbd0GUHeVyaX',
-}
+};
 
-module.exports = twitterKeys;
+var spotifKey = {
+  id: '4011a7ff05e442af8f826fde23f7b01f',
+  secret: 'aafa088b788c4e67b6b269b5ecb627cd'
+};  
+
+var omdbKey = {
+    API_key: '40e9cece'
+};
+
+var apiKeys = {
+    twitter: twitterKeys,
+    spotify: spotifKey,
+    omdb: omdbKey
+}
+module.exports = apiKeys;
--- liri.js
+++ liri.js
@@ -1,63 +1,141 @@
 var myKeys = require('./keys.js');
 var Twitter = require('twitter');
-var Spotify = require('spotify');
+var Spotify = require('node-spotify-api');
+var request = require("request");
 var fs = require("fs");
-var client = new Twitter(myKeys);
-
-
 
 //switch format
 var command = process.argv[2];
-var song = process.argv[3];
+var input = process.argv[3];
+var searchInput = process.argv;
+
+for (var i = 4; i<searchInput.length; i++){
+    input += '+' + searchInput[i];
+}
 
 switch(command){
     default:
-        console.log("type out following options: my-tweets, spotify-this-song, movie-this");
+        console.log("type out following options: my-tweets, spotify-this-song, movie-this, do-what-it-says");
         break;
     case "my-tweets": 
         showTweets(); 
         break;
     case "spotify-this-song":
-        showSpotify();
+        showSpotify(input);
         break;
     case "movie-this":
-        showMovie(x);
+        showMovie(input);
         break;
-              }
+    case 'do-what-it-says':
+        showThis();
+        break;
+              };
 
 
 //npm install twitter
 function showTweets(){
-    var params = {screen_name: 'paf2200'};
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    var params = {screen_name: 'paf2200', count:20};
+    var client = new Twitter({
+        consumer_key: myKeys.twitter.consumer_key,
+        consumer_secret: myKeys.twitter.consumer_secret,
+        access_token_key: myKeys.twitter.access_token_key,
+        access_token_secret: myKeys.twitter.access_token_secret
+    });
+    /*    var client = new Twitter(myKeys.twitter)*/
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
-            for (var i = 0; i < 20; i++){
-            var tweetMsg = {
-                        "Name"      : tweets[i].user.name,
-                        "Created"   : tweets[i].created_at,
-                        "Tweet"     : tweets[i].text
-            };
-                
-                console.log(tweetMsg);
+            for (var i=0; i < tweets.length; i++) {
+                console.log(
+                    "------------------------------------\n"+
+                    tweets[i].user.name +"\n"+ tweets[i].created_at +"\n"+ tweets[i].text + "\n------------------------------------");
             }
-        } else {
-         console.log('thiere is an error');
+        }
+        else {
+            console.log('thiere is an error: ' + error);
         }
     });
 };
-
 //npm install spotify
-function showSpotify(){
+function showSpotify(input){
+    var mySpot = new Spotify({
+        id: myKeys.spotify.id,
+        secret: myKeys.spotify.secret
+    });
+   
+    if(!input){
+        input = "hello";
+    } 
+    mySpot.search({type: 'track', query: input, limit:5}, function (err, data) {
+        if (err) {
+            console.log("There is an error: " + err);
+        } else {
+            for(var i = 0; i < data.tracks.items.length; i++){
+                var datae = data.tracks.items[i];
+                console.log("Artist: " + datae.artists[0].name);
+                console.log("Song: " + datae.name);
+                console.log("Preview URL: " + datae.preview_url);
+                console.log("Album: " + datae.album.name);
+                console.log("-----------------------");
+                
+                fs.appendFile('log.txt', input);
+                fs.appendFile('log.txt', datae.artists[0].name);
+                fs.appendFile('log.txt', datae.name);
+                fs.appendFile('log.txt', datae.preview_url);
+                fs.appendFile('log.txt', datae.album.name);
+            }
+        };
+    });
+};
 
-var spotify = new Spotify({
-  id: '4011a7ff05e442af8f826fde23f7b01f',
-  secret: 'aafa088b788c4e67b6b269b5ecb627cd'
-});    
-console.log(Spotify);
-Spotify.search({ type: track , query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  } 
- 
-console.log(data); 
-});
+
+function showMovie(input){
+    var moviekey = "40e9cece";
+    if(!input) {
+        input = "Mr. Nobody";
+    } 
+    var omdbURL = 'http://www.omdbapi.com/?apikey='+moviekey+'&t=' + input + '&plot=short&tomatoes=true';
+/*http://www.omdbapi.com/?apikey=40e9cece&t=stars+war&plot=short&tomatoes=true*/
+   
+    request(omdbURL, function (error, response, body){
+        if(!error && response.statusCode == 200){
+            var body = JSON.parse(body);
+
+            console.log("Title: " + body.Title);
+            console.log("Release Year: " + body.Year);
+            console.log("IMdB Rating: " + body.imdbRating);
+            console.log("Country: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+            console.log("Rotten Tomatoes URL: " + body.tomatoURL); 
+            
+            fs.appendFile("log.txt", body.Title);
+            fs.appendFile("log.txt", body.Year);
+            fs.appendFile("log.txt", body.imdbRating);
+            fs.appendFile("log.txt", body.Country);
+            fs.appendFile("log.txt", body.Language);
+            fs.appendFile("log.txt", body.Actors);
+            fs.appendFile("log.txt", body.tomatoRating);
+            fs.appendFile("log.txt", body.tomatoURL);
+        } else {
+            console.log(error)
+        }
+    });
+};    
+
+function showThis() {
+    fs.readFile('random.txt', 'utf8', function(error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            var dataArr = data.split(',');
+            if (dataArr[0] === 'spotify-this-song') {
+               showSpotify(dataArr[1]);
+            }
+            if (dataArr[0] === 'movie-this') {
+                showMovie(dataArr[1]);
+            }
+        }
+    });
+}
